cc2da85ab45d425e1c6e0cf7a15d7e3d3d0b0175
Wytze Bruinsma <wytze@wytzejelle.nl>
2018-02-06 16:00:56 +0100

    test change 3

--- TeamcityTest/TeamcityTest/Program.cs
+++ TeamcityTest/TeamcityTest/Program.cs
@@ -7,6 +7,7 @@ namespace TeamcityTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
