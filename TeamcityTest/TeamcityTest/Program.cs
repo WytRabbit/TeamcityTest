8af902a31568bf71b50d8a42087c8628cbf212ff
Wytze Bruinsma <wytze@wytzejelle.nl>
2018-02-06 16:59:29 +0100

    test

--- TeamcityTest/TeamcityTest/Program.cs
+++ TeamcityTest/TeamcityTest/Program.cs
@@ -8,6 +8,7 @@ namespace TeamcityTest
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
